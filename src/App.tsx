abcaeef2237cfcb8f950abec8181c180cee04496
Mihail Marian <mihailmarian12@gmail.com>
2022-11-20 08:59:09 +0000

    added elements

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button>Connect wallet</button>
+        <button className="connect-wallet">Connect wallet</button>
         <div>Total balance:</div>
       </header>
     </div>
