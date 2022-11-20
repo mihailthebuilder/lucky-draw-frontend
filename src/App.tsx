ada834d058d52ce15e26f8f4cc7b644d1ad2aba1
Mihail Marian <mihailmarian12@gmail.com>
2022-11-20 11:11:08 +0000

    fetch window with ethereum wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
+import { providers } from "ethers";
+
+type WindowWithWallet = {
+  ethereum: providers.ExternalProvider;
+};
+
+const getEthereumObject = (window: WindowWithWallet) => window.ethereum;
+
 function App() {
+  useEffect(() => {
+    console.log(getEthereumObject(window as any as WindowWithWallet));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
