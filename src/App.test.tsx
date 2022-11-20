6a08b9b86fc34408a0179457dabd7002c81d4fdb
Mihail Marian <mihailmarian12@gmail.com>
2022-11-20 08:26:18 +0000

    remove boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const connectWalletButton = screen.getByText(/connect wallet/i);
+  expect(connectWalletButton.tagName.toLowerCase()).toEqual("button");
+
+  screen.getByText(/total balance:/i);
 });
