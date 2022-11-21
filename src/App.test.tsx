900c3667f66dfacb91663fd3a26e7c62d029e677
Mihail Marian <mihailmarian12@gmail.com>
2022-11-21 08:59:02 +0000

    works but happy path tests fails for some reason

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
 import { windowWithEthereumWallet } from "./windowWithEthereumWallet";
@@ -13,12 +13,27 @@ describe("given browser doesn't have ethereum object in window", () => {
   });
 });
 
-describe("given browser has MetaMask wallet", () => {
+describe("given browser has ethereum object but no wallet", () => {
   beforeAll(() => {
-    windowWithEthereumWallet.ethereum = { isMetaMask: true };
+    windowWithEthereumWallet.ethereum = {};
   });
 
-  test("render happy path", () => {
+  test("render message asking for the wallet", () => {
+    render(<App />);
+    screen.getByText(
+      /please add MetaMask to your browser and set up a wallet/i
+    );
+  });
+});
+
+describe("given browser has MetaMask wallet", () => {
+  beforeAll(() => {
+    windowWithEthereumWallet.ethereum = {
+      request: jest.fn(() => Promise.resolve({ data: ["hello"] })),
+    };
+  });
+
+  test("render happy path", async () => {
     render(<App />);
 
     const connectWalletButton = screen.getByText(/connect wallet/i);
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import { windowWithEthereumWallet } from "./windowWithEthereumWallet";
 
+type AccountsInBrowser = string[];
+
 const getEthereumObjectFromWindow = () => windowWithEthereumWallet.ethereum;
 
+const findMetamaskAccount: () => Promise<string> = async () => {
+  const eth = getEthereumObjectFromWindow();
+  if (!eth) {
+    throw "No Ethereum object in window";
+  }
+
+  if (!eth.request) {
+    throw "Invalid Ethereum object in window";
+  }
+
+  const accounts = (await eth.request({
+    method: "eth_accounts",
+  })) as AccountsInBrowser;
+
+  console.log(accounts);
+
+  if (accounts.length == 0) {
+    throw "No authorised account found";
+  }
+
+  return accounts[0];
+};
+
 function App() {
+  const [metamaskAccount, setMetamaskAccount] = useState<string>();
+
+  useEffect(() => {
+    findMetamaskAccount()
+      .then((account) => {
+        setMetamaskAccount(account);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        {getEthereumObjectFromWindow() ? (
+        {metamaskAccount ? (
           <>
             <button className="connect-wallet">Connect wallet</button>
             <div>Total balance:</div>
